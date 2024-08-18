43cc59a0692ac1e8aba2294e60e4b88a18a53735
webtechmoz <zoidycine@gmail.com>
2024-08-18 17:41:01 +0200

    manage-sql v0.5.5

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='manage-sql',
-    version='0.5.4',
+    version='0.5.5',
     author='Web Tech Moz',
     author_email='zoidycine@gmail.com',
     description='Biblioteca Python para gestão de bases de dados SQLite, MYSQL e PostgreSQL com maior eficiência',
@@ -13,14 +13,18 @@ setup(
     keywords=['manage-sql', 'sqlite', 'sqlite manager', 'mysql', 'mysql manager', 'mysql python', 'mysql connector', 'postgresql connector', 'postgresql', 'postgresql python'],
     license='MIT',
     classifiers=[
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.11',
         'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.6',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
     ],
     install_requires=[
         "mysql-connector",
         "mysql-connector-python",
-        "psycopg2-binary"
+        "psycopg2"
     ],
     python_requires='>=3.6',
 )
